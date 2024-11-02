cd18c6d308d8f3759ed815bfba76999013861ac2
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-11-01 20:31:48 -0400

    modify pipfile

--- script.py
+++ script.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     nltk.download('wordnet')
     nltk.download('punkt')
     
-    spacy.cli.download('en_core_web_trf')
     
     model_name = "facebook/bart-large-mnli"
     AutoTokenizer.from_pretrained(model_name)
